5a0b1500d3db6cbf439dec442aee9d76adfefb24
RayChen29 <rcn9666@gmail.com>
2026-01-28 22:16:39 -0800

    Update vite.config.ts

--- karutag/vite.config.ts
+++ karutag/vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // Allow overriding the base path via the BASE_URL env var (useful for GitHub Pages)
-  base: process.env.BASE_URL || '/',
+  // base: process.env.BASE_URL || '/',
+  base: '/',   
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        // base: "/",
       },
     }),
   ],
